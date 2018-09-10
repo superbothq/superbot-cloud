4ac27b4b0e5308282803869b4097d98ff79da5c1
DeeMak13 <dima.m@active-bridge.com>
2018-09-10 16:56:37 +0300

    Move credentials file path to constants, move constants under Superbot::Cloud namespace

--- lib/superbot/cloud.rb
+++ lib/superbot/cloud.rb
@@ -1,5 +1,10 @@
 module Superbot
   module Cloud
+    CLOUD_URI = 'https://superapp-staging.herokuapp.com/login/cloud'.freeze
+    CLOUD_API_URI = 'https://superapp-staging.herokuapp.com/api/v1/sessions'.freeze
+    CLOUD_API_TOKEN_URI = 'https://superapp-staging.herokuapp.com/api/v1/token'.freeze
+    CREDENTIALS_PATH = File.join(ENV['HOME'], '.superbot').freeze
+    CREDENTIALS_FILE_PATH = File.join(CREDENTIALS_PATH, 'cloud_token.json').freeze
   end
 end
 
--- lib/superbot/cloud/cli/cloud/login_command.rb
+++ lib/superbot/cloud/cli/cloud/login_command.rb
@@ -6,17 +6,12 @@ module Superbot
   module CLI
     module Cloud
       class LoginCommand < Clamp::Command
-        CLOUD_URI = 'https://superapp-staging.herokuapp.com/login/cloud'.freeze
-        CLOUD_API_URI = 'https://superapp-staging.herokuapp.com/api/v1/sessions'.freeze
-        CLOUD_API_TOKEN_URI = 'https://superapp-staging.herokuapp.com/api/v1/token'.freeze
-        CREDENTIALS_FILE_PATH ="#{ENV['HOME']}/.superbot/cloud_token.json".freeze
-
         option ['-i', '--interactive'], :flag, 'interactive login from command line'
         option ['-f', '--force'], :flag, 'force override current credentials'
 
         def execute
-          if File.exist?(CREDENTIALS_FILE_PATH) && !force?
-            credentials = JSON.parse(File.read(CREDENTIALS_FILE_PATH))
+          if File.exist?(Superbot::Cloud::CREDENTIALS_FILE_PATH) && !force?
+            credentials = JSON.parse(File.read(Superbot::Cloud::CREDENTIALS_FILE_PATH))
 
             if credentials_login_request(credentials).code == '200'
               puts "Logged in as #{credentials['email']}"
@@ -29,13 +24,13 @@ module Superbot
         end
 
         def cloud_uri
-          URI.parse(CLOUD_URI).tap do |uri|
+          URI.parse(Superbot::Cloud::CLOUD_URI).tap do |uri|
             uri.query = URI.encode_www_form(redirect_uri: 'http://localhost:4567/login')
           end
         end
 
         def credentials_login_request(credentials)
-          uri = URI.parse(CLOUD_API_TOKEN_URI)
+          uri = URI.parse(Superbot::Cloud::CLOUD_API_TOKEN_URI)
           req = Net::HTTP::Post.new(uri)
           req['Authorization'] = format('Token email=%<email>s, token="%<token>s"', email: credentials['email'], token: credentials['token'])
           res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
@@ -47,7 +42,7 @@ module Superbot
           email = (print 'Email: '; $stdin.gets.rstrip)
           password =  (print 'Password: '; $stdin.gets.rstrip)
 
-          uri = URI.parse(CLOUD_API_URI)
+          uri = URI.parse(Superbot::Cloud::CLOUD_API_URI)
           req = Net::HTTP::Post.new(uri)
           req.content_type = 'multipart/form-data'
           req.set_form_data(email: email, password: password)
@@ -59,9 +54,8 @@ module Superbot
 
           return parsed_body['errors'] if parsed_body['errors']
 
-          path = "#{ENV['HOME']}/.superbot"
-          FileUtils.mkdir_p path, 775
-          File.write File.join(path, 'cloud_token.json'), res.body
+          FileUtils.mkdir_p Superbot::Cloud::CREDENTIALS_PATH, 775
+          File.write Superbot::Cloud::CREDENTIALS_FILE_PATH, res.body
           puts "Logged in as #{parsed_body['email']}"
         end
 
