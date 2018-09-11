34c2b6d3907eb772140e958742993aa6ad8e4652
DeeMak13 <dima.m@active-bridge.com>
2018-09-11 13:45:16 +0300

    Add organization list subcommand

---
+++ lib/superbot/cloud/cli/cloud/organization/list_command.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'net/http'
+require 'launchy'
+
+module Superbot
+  module CLI
+    module Cloud
+      module Organization
+        class ListCommand < Clamp::Command
+
+          def execute
+            abort "You are not logged in, use `superbot cloud login` to login" unless Superbot::Cloud.credentials
+            list_organizations
+          end
+
+          def list_organizations
+            res = Superbot::Cloud::Api.request(:organization_list)
+            parsed_body = JSON.parse(res.body, symbolize_names: true)
+            abort parsed_body[:errors] unless res.code == '200'
+            parsed_body.each do |organization|
+              puts organization[:name]
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/superbot/cloud/cli/cloud/organization_command.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative 'organization/list_command'
+
+module Superbot
+  module CLI
+    module Cloud
+      class OrganizationCommand < Clamp::Command
+        subcommand ['list'], "List user organizations from the cloud", Cloud::Organization::ListCommand
+
+        def self.run
+          super
+        rescue StandardError => exc
+          warn exc.message
+          warn exc.backtrace.join("\n")
+        end
+      end
+    end
+  end
+end
