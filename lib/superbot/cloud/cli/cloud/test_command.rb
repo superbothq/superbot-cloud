d9d0168bac7d17b1c99442ebd77249c9d1340649
DeeMak13 <dima.m@active-bridge.com>
2018-09-27 14:58:32 +0300

    Add cloud test delete subcommand

--- lib/superbot/cloud/cli/cloud/test_command.rb
+++ lib/superbot/cloud/cli/cloud/test_command.rb
@@ -2,13 +2,15 @@
 
 require_relative 'test/list_command'
 require_relative 'test/upload_command'
+require_relative 'test/delete_command'
 
 module Superbot
   module CLI
     module Cloud
       class TestCommand < Clamp::Command
         subcommand ['list'], "List user tests from the cloud", Cloud::Test::ListCommand
-        subcommand ['upload'], "Upload tests to the cloud", Cloud::Test::UploadCommand
+        subcommand ['upload'], "Upload test to the cloud", Cloud::Test::UploadCommand
+        subcommand ['delete'], "Delete test from the cloud", Cloud::Test::DeleteCommand
 
         def self.run
           super
