0a5860f2ab146606f97965536379ddc6da555c5a
DeeMak13 <dima.m@active-bridge.com>
2018-09-27 14:35:36 +0300

    Refactor Api module to support delete endpoint and abort if request errored

--- lib/superbot/cloud/api.rb
+++ lib/superbot/cloud/api.rb
@@ -5,23 +5,24 @@ require 'net/http/post/multipart'
 module Superbot
   module Cloud
     module Api
-      BASE_URI = "https://superapp-staging.herokuapp.com/api/v1/"
+      BASE_URI = "https://superapp-staging.herokuapp.com/api/v1"
       ENDPOINT_MAP = {
         login:             { method: :post, endpoint: 'sessions' },
         token:             { method: :post, endpoint: 'token' },
         organization_list: { method: :get, endpoint: 'organizations' },
         test_list:         { method: :get, endpoint: 'tests' },
-        test_upload:       { method: :post_multipart, endpoint: 'tests' }
+        test_upload:       { method: :post_multipart, endpoint: 'tests' },
+        delete_test:       { method: :delete, endpoint: 'tests', required_param: :name }
       }.freeze
 
       def self.request(type, params: {})
-        method = ENDPOINT_MAP.dig(type, :method).to_s
-        uri = URI.join(BASE_URI, ENDPOINT_MAP.dig(type, :endpoint))
+        method, endpoint, required_param = ENDPOINT_MAP[type].values
+        uri = URI.parse([BASE_URI, endpoint, params[required_param]].compact.join('/'))
 
         req = Net::HTTP.const_get(
-          method.split('_').map(&:capitalize).join('::')
+          method.to_s.split('_').map(&:capitalize).join('::')
         ).new(uri, params.compact)
-        req.set_form_data(params.compact) unless method == 'post_multipart'
+        req.set_form_data(params.compact) unless method == :post_multipart
         if Superbot::Cloud.credentials
           req['Authorization'] = format(
             'Token token="%<token>s", email="%<email>s"',
@@ -29,9 +30,13 @@ module Superbot
           )
         end
 
-        Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
           http.request(req)
         end
+        parsed_response = response.class.body_permitted? && JSON.parse(response.body, symbolize_names: true) || {}
+        return parsed_response if response.is_a? Net::HTTPSuccess
+
+        abort parsed_response[:errors]
       end
     end
   end
