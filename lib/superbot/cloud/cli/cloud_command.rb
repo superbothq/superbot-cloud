99531bc6e9b7502bec3c813d091450fc2a3de2c2
DeeMak13 <dima.m@active-bridge.com>
2018-10-24 22:38:53 +0300

    Add cloud run command

--- lib/superbot/cloud/cli/cloud_command.rb
+++ lib/superbot/cloud/cli/cloud_command.rb
@@ -5,14 +5,16 @@ require_relative 'cloud/login_command'
 require_relative 'cloud/version_command'
 require_relative 'cloud/organization_command'
 require_relative 'cloud/test_command'
+require_relative 'cloud/run_command'
 
 module Superbot
   module CLI
     class CloudCommand < Clamp::Command
-      subcommand ['version'], "Superbot cloud version", Cloud::VersionCommand
-      subcommand ['login'], "Login to superbot cloud", Cloud::LoginCommand
-      subcommand ['org'], "Manage your organizations", Cloud::OrganizationCommand
+      subcommand ['version'], 'Superbot cloud version', Cloud::VersionCommand
+      subcommand ['login'], 'Login to superbot cloud', Cloud::LoginCommand
+      subcommand ['org'], 'Manage your organizations', Cloud::OrganizationCommand
       subcommand ['test'], "Manage your tests", Cloud::TestCommand
+      subcommand ['run'], "Run your test in the cloud", Cloud::RunCommand
 
       def self.run
         super
