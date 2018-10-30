e8fe602a1e20545df0e86cf54388df8ef5653939
DeeMak13 <dima.m@active-bridge.com>
2018-10-30 18:00:11 +0200

    Remove cloud run command

--- lib/superbot/cloud.rb
+++ lib/superbot/cloud.rb
@@ -4,16 +4,9 @@ module Superbot
   module Cloud
     LOGIN_URI = 'http://www.superbot.cloud/login/cloud'
     CREDENTIALS_PATH = File.join(Dir.home, '.superbot')
+    private_constant :CREDENTIALS_PATH
     CREDENTIALS_FILE_PATH = File.join(CREDENTIALS_PATH, 'cloud_token.json')
-    SCREENSHOTS_BASE_URL = {
-      local: "http://localhost:3002/v1",
-      remote: "http://peek.superbot.cloud/v1"
-    }.freeze
-    WEBDRIVER_URL = {
-      local: "http://localhost:3000/webdriver/v1",
-      remote: "http://webdriver.superbot.cloud:3000/webdriver/v1"
-    }.freeze
-    WEBDRIVER_TYPE = :remote # use :local if you are running superbot webdriver cluster locally
+    private_constant :CREDENTIALS_FILE_PATH
 
     def self.credentials
       return unless File.exist?(CREDENTIALS_FILE_PATH)
@@ -28,14 +21,6 @@ module Superbot
       "Logged in as #{data[:email]}".tap do |message|
         puts message
       end
-    end
-
-    def self.webdriver_url
-      WEBDRIVER_URL[WEBDRIVER_TYPE]
-    end
-
-    def self.screenshots_url(session_id)
-      "#{SCREENSHOTS_BASE_URL[WEBDRIVER_TYPE]}/#{session_id}"
     end
   end
 end
