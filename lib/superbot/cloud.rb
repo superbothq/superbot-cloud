23137bc5430e759cf51688d363228f4c6150d947
DeeMak13 <dima.m@active-bridge.com>
2018-11-06 11:32:10 +0200

    Fix saving credentials

--- lib/superbot/cloud.rb
+++ lib/superbot/cloud.rb
@@ -16,8 +16,8 @@ module Superbot
 
     def self.save_credentials(data)
       data.transform_keys!(&:to_sym)
-      FileUtils.mkdir_p Superbot::Cloud::CREDENTIALS_PATH
-      File.write Superbot::Cloud::CREDENTIALS_FILE_PATH, data.to_json
+      FileUtils.mkdir_p CREDENTIALS_PATH
+      File.write CREDENTIALS_FILE_PATH, data.to_json
       "Logged in as #{data[:email]}".tap do |message|
         puts message
       end
