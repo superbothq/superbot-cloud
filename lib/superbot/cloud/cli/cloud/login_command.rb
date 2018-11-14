8286982bb960c51b36677b360809db03ce1cb218
DeeMak13 <dima.m@active-bridge.com>
2018-11-14 15:16:15 +0200

    Exit right after user successfully logged in

--- lib/superbot/cloud/cli/cloud/login_command.rb
+++ lib/superbot/cloud/cli/cloud/login_command.rb
@@ -15,8 +15,9 @@ module Superbot
 
           begin
             Superbot::Cloud::Api.request(:token)
-            puts "Logged in as #{Superbot::Cloud.credentials[:email]}"
-          rescue SystemExit => e
+            user_creds = Superbot::Cloud.credentials
+            puts "Logged in as #{user_creds[:username]} (#{user_creds[:email]})"
+          rescue SystemExit => e.message
             abort unless e.message == 'Invalid credentials'
             proceed_to_login
           end
@@ -34,20 +35,37 @@ module Superbot
         end
 
         def web_login
-          Superbot::Cloud::WebLogin.run!
-          Launchy.open(cloud_login_uri)
-
-          puts "Your browser has been opened to visit:"
-          puts cloud_login_uri
-          puts ""
-          puts "Press enter to exit"
-          $stdin.gets
+          web = Superbot::Cloud::WebLogin.new
+          web.run_async_after_running!
+          open_cloud_login_uri
+          handle_keyboard_interrupt
+          wait_for_login(web)
         end
 
-        def cloud_login_uri
-          URI.parse(Superbot::Cloud::LOGIN_URI).tap do |uri|
+        def open_cloud_login_uri
+          cloud_login_uri = URI.parse(Superbot::Cloud::LOGIN_URI).tap do |uri|
             uri.query = URI.encode_www_form(redirect_uri: 'http://localhost:4567/login')
           end.to_s
+
+          Launchy.open(cloud_login_uri)
+          puts "Your browser has been opened to visit:", cloud_login_uri
+          puts
+        end
+
+        def handle_keyboard_interrupt
+          trap "SIGINT" do
+            puts
+            puts "Command killed by keyboard interrupt"
+            exit 130
+          end
+        end
+
+        def wait_for_login(web)
+          loop do
+            break unless web.running?
+
+            sleep 0.1
+          end
         end
       end
     end
--- lib/superbot/cloud/web_login.rb
+++ lib/superbot/cloud/web_login.rb
@@ -14,10 +14,12 @@ module Superbot
           set :silent_webrick, true
           set :silent_access_log, false
           server_settings[:Silent] = true
+          instance = self
 
           get "/login" do
             credentials = request.params.slice('username', 'email', 'token')
             Superbot::Cloud.save_credentials(credentials)
+            instance.quit!
             redirect "#{Superbot::Cloud::LOGIN_URI}/success?username=#{Superbot::Cloud.credentials[:username]}"
           end
         end
@@ -28,8 +30,22 @@ module Superbot
       end
 
       def run!
+        @sinatra.run_async!
+      end
+
+      def running?
+        @sinatra.running?
+      end
+
+      def run_async_after_running!
         Thread.new do
           @sinatra.run!
+        end
+
+        loop do
+          break if running?
+
+          sleep 0.001
         end
       end
     end
