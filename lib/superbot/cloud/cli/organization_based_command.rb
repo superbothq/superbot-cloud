3aa9262691fee3ab8e266c28a5918666ad4c2531
DeeMak13 <dima.m@active-bridge.com>
2019-01-10 17:51:37 +0200

    Make --org option required

--- lib/superbot/cloud/cli/organization_based_command.rb
+++ lib/superbot/cloud/cli/organization_based_command.rb
@@ -4,7 +4,7 @@ module Superbot
   module Cloud
     module CLI
       class OrganizationBasedCommand < LoginRequiredCommand
-        option ["--org"], "ORGANIZATION", "Organization to to take actions on", attribute_name: :organization
+        option ["--org"], "ORGANIZATION", "Name of organization to take action on", required: true, attribute_name: :organization
       end
     end
   end
