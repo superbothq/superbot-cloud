7183637fd1c8e6bdf744d7791a03dac2a934daf7
DeeMak13 <dima.m@active-bridge.com>
2019-01-17 17:11:42 +0200

    Add --quiet option for test list, support multiple values for delete

--- lib/superbot/cloud/cli/test/delete_command.rb
+++ lib/superbot/cloud/cli/test/delete_command.rb
@@ -5,15 +5,19 @@ module Superbot
     module CLI
       module Test
         class DeleteCommand < BaseCommand
-          parameter "NAME", "the name of the test to delete", required: true
+          parameter "NAME ...", "the name of the test to delete", required: true
 
           def execute
             delete_test
           end
 
           def delete_test
-            Superbot::Cloud::Api.request(:delete_test, params: { name: name, organization_name: organization })
-            puts "Tests successfully deleted"
+            name_list.each do |name|
+              Superbot::Cloud::Api.request(:delete_test, params: { name: name, organization_name: organization })
+              puts "Tests #{name} successfully deleted"
+            rescue SystemExit => e
+              puts "Test #{name} removal failed: #{e.message}"
+            end
           end
         end
       end
--- lib/superbot/cloud/cli/test/list_command.rb
+++ lib/superbot/cloud/cli/test/list_command.rb
@@ -5,17 +5,22 @@ module Superbot
     module CLI
       module Test
         class ListCommand < BaseCommand
+          option %w[-q --quiet], :flag, "Only show test names"
+
           def execute
             list_tests
           end
 
           def list_tests
             api_response = Superbot::Cloud::Api.request(:test_list, params: { organization_name: organization })
-            abort api_response[:error] if api_response[:error]
-            puts "Organization: #{api_response[:organization]}"
-            puts "Tests:"
-            api_response[:tests].each do |test|
-              puts(test[:name], test[:files].map { |f| "- #{f[:filename]}" })
+            if quiet?
+              puts(api_response[:tests].map { |test| test[:name] })
+            else
+              puts "Organization: #{api_response[:organization]}"
+              puts "Tests:"
+              api_response[:tests].each do |test|
+                puts(test[:name], test[:files].map { |f| "- #{f[:filename]}" })
+              end
             end
           end
         end
